21964840346102e121f1389cb102ecb963a5d1fc
Luis Valdez <lvaldez@attivio.com>
2018-02-15 13:52:19 -0500

    PLAT-41220: Check for the absence of a mapbox key in the configuration and disply a message instead of the map.

--- src/components/MapFacetContents.js
+++ src/components/MapFacetContents.js
@@ -13,6 +13,7 @@ import Configurable from '../components/Configurable';
 import SearchFacetBucket from '../api/SearchFacetBucket';
 import PositionUtils from '../util/PositionUtils';
 import ObjectUtils from '../util/ObjectUtils';
+import StringUtils from '../util/StringUtils';
 
 type MapFacetContentsProps = {
   /** The facet’s buckets. */
@@ -180,147 +181,154 @@ class MapFacetContents extends React.Component<MapFacetContentsDefaultProps, Map
   }
 
   render() {
-    const Map = ReactMapboxGl({
-      accessToken: this.props.mapboxKey,
-      attributionControl: false,
-    });
+    if (StringUtils.notEmpty(this.props.mapboxKey)) {
+      const Map = ReactMapboxGl({
+        accessToken: this.props.mapboxKey,
+        attributionControl: false,
+      });
 
-    const points = this.props.buckets.map((bucket) => {
-      const value = bucket.value; // JSON.parse(bucket.value);
-      // Keep track of the boundaries of the coordinates
-      return (
-        <Marker
-          coordinates={[value.longitude || 0, value.latitude || 0]}
-          onClick={() => {
-            this.props.addFacetFilter(bucket);
-          }}
-          key={`${value.longitude || 0},${value.latitude || 0}`}
-          style={{ cursor: 'pointer' }}
-        >
-          <Glyphicon glyph="map-marker" style={{ fontSize: '18px', color: '#2a689c' }} />
-        </Marker>
-      );
-    });
-
-    const height = this.props.size && this.props.size.height ? this.props.size.height : 400;
-    const width = this.props.size && this.props.size.width ? this.props.size.width : 400;
-
-    // style filters
-    const selected = ['==', 'active', 'true'];
-    const deselected = ['==', 'active', 'false'];
-    const polygon = ['==', '$type', 'Polygon'];
-    const line = ['==', '$type', 'LineString'];
-
-    // styles
-    const selectedColor = '#f9c448';
-    const deselectedColor = '#3276b1';
-    const lineLayout = {
-      'line-cap': 'round',
-      'line-join': 'round',
-    };
-    const selectedLinePaint = {
-      'line-color': selectedColor,
-      'line-dasharray': [0.2, 2],
-      'line-width': 2,
-    };
-
-    return (
-      <div>
-        <a
-          className="btn btn-primary btn-xs"
-          style={{
-            position: 'relative',
-            display: 'inline',
-            zIndex: 1,
-            top: '30px',
-            left: '5px',
-          }}
-          role="button"
-          tabIndex={0}
-          onClick={this.apply}
-        >
-          Update Results
-        </a>
-        <Map
-          style="mapbox://styles/mapbox/light-v9" // eslint-disable-line react/style-prop-object
-          containerStyle={{
-            height,
-            width,
-          }}
-          center={[this.state.longitude || 0, this.state.latitude || 0]}
-          zoom={[this.state.zoom || 1]}
-          onStyleLoad={(map: any) => {
-            // add a filter query to the state on draw.create
-            map.on('draw.create', this.create);
-            // find and remove the appropriate filter query from the state on draw.delete
-            map.on('draw.delete', this.delete);
-            // keep track of the selected polygon whenever the selection changes
-            map.on('draw.selectionchange', this.select);
-            // find and remove the selected filter from the state then add the updated filter back to it on draw.update
-            map.on('draw.update', this.update);
-          }}
-        >
-          <ZoomControl position="bottom-right" />
-          <DrawControl
-            controls={{
-              point: false,
-              line_string: false,
-              polygon: true,
-              trash: true,
-              combine_features: false,
-              uncombine_features: false,
+      const points = this.props.buckets.map((bucket) => {
+        const value = bucket.value; // JSON.parse(bucket.value);
+        // Keep track of the boundaries of the coordinates
+        return (
+          <Marker
+            coordinates={[value.longitude || 0, value.latitude || 0]}
+            onClick={() => {
+              this.props.addFacetFilter(bucket);
             }}
-            styles={[{
-              id: 'selected-line',
-              type: 'line',
-              filter: ['all', line, selected],
-              layout: lineLayout,
-              paint: selectedLinePaint,
-            }, {
-              id: 'selected-polygon-fill',
-              type: 'fill',
-              filter: ['all', polygon, selected],
-              paint: {
-                'fill-color': selectedColor,
-                'fill-outline-color': selectedColor,
-                'fill-opacity': 0.1,
-              },
-            }, {
-              id: 'selected-polygon-outline',
-              type: 'line',
-              filter: ['all', polygon, selected],
-              layout: lineLayout,
-              paint: selectedLinePaint,
-            }, {
-              id: 'vertex',
-              type: 'circle',
-              filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point']],
-              paint: {
-                'circle-radius': 5,
-                'circle-color': selectedColor,
-              },
-            }, {
-              id: 'deselected-polygon-fill',
-              type: 'fill',
-              filter: ['all', polygon, deselected],
-              paint: {
-                'fill-color': deselectedColor,
-                'fill-outline-color': deselectedColor,
-                'fill-opacity': 0.1,
-              },
-            }, {
-              id: 'deselected-polygon-outline',
-              type: 'line',
-              filter: ['all', polygon, deselected],
-              layout: lineLayout,
-              paint: {
-                'line-color': deselectedColor,
-                'line-width': 3,
-              },
-            }]}
-          />
-          {points}
-        </Map>
+            key={`${value.longitude || 0},${value.latitude || 0}`}
+            style={{ cursor: 'pointer' }}
+          >
+            <Glyphicon glyph="map-marker" style={{ fontSize: '18px', color: '#2a689c' }} />
+          </Marker>
+        );
+      });
+
+      const height = this.props.size && this.props.size.height ? this.props.size.height : 400;
+      const width = this.props.size && this.props.size.width ? this.props.size.width : 400;
+
+      // style filters
+      const selected = ['==', 'active', 'true'];
+      const deselected = ['==', 'active', 'false'];
+      const polygon = ['==', '$type', 'Polygon'];
+      const line = ['==', '$type', 'LineString'];
+
+      // styles
+      const selectedColor = '#f9c448';
+      const deselectedColor = '#3276b1';
+      const lineLayout = {
+        'line-cap': 'round',
+        'line-join': 'round',
+      };
+      const selectedLinePaint = {
+        'line-color': selectedColor,
+        'line-dasharray': [0.2, 2],
+        'line-width': 2,
+      };
+
+      return (
+        <div>
+          <a
+            className="btn btn-primary btn-xs"
+            style={{
+              position: 'relative',
+              display: 'inline',
+              zIndex: 1,
+              top: '30px',
+              left: '5px',
+            }}
+            role="button"
+            tabIndex={0}
+            onClick={this.apply}
+          >
+            Update Results
+          </a>
+          <Map
+            style="mapbox://styles/mapbox/light-v9" // eslint-disable-line react/style-prop-object
+            containerStyle={{
+              height,
+              width,
+            }}
+            center={[this.state.longitude || 0, this.state.latitude || 0]}
+            zoom={[this.state.zoom || 1]}
+            onStyleLoad={(map: any) => {
+              // add a filter query to the state on draw.create
+              map.on('draw.create', this.create);
+              // find and remove the appropriate filter query from the state on draw.delete
+              map.on('draw.delete', this.delete);
+              // keep track of the selected polygon whenever the selection changes
+              map.on('draw.selectionchange', this.select);
+              // find and remove the selected filter from the state then add the updated filter back to it on draw.update
+              map.on('draw.update', this.update);
+            }}
+          >
+            <ZoomControl position="bottom-right" />
+            <DrawControl
+              controls={{
+                point: false,
+                line_string: false,
+                polygon: true,
+                trash: true,
+                combine_features: false,
+                uncombine_features: false,
+              }}
+              styles={[{
+                id: 'selected-line',
+                type: 'line',
+                filter: ['all', line, selected],
+                layout: lineLayout,
+                paint: selectedLinePaint,
+              }, {
+                id: 'selected-polygon-fill',
+                type: 'fill',
+                filter: ['all', polygon, selected],
+                paint: {
+                  'fill-color': selectedColor,
+                  'fill-outline-color': selectedColor,
+                  'fill-opacity': 0.1,
+                },
+              }, {
+                id: 'selected-polygon-outline',
+                type: 'line',
+                filter: ['all', polygon, selected],
+                layout: lineLayout,
+                paint: selectedLinePaint,
+              }, {
+                id: 'vertex',
+                type: 'circle',
+                filter: ['all', ['==', 'meta', 'vertex'], ['==', '$type', 'Point']],
+                paint: {
+                  'circle-radius': 5,
+                  'circle-color': selectedColor,
+                },
+              }, {
+                id: 'deselected-polygon-fill',
+                type: 'fill',
+                filter: ['all', polygon, deselected],
+                paint: {
+                  'fill-color': deselectedColor,
+                  'fill-outline-color': deselectedColor,
+                  'fill-opacity': 0.1,
+                },
+              }, {
+                id: 'deselected-polygon-outline',
+                type: 'line',
+                filter: ['all', polygon, deselected],
+                layout: lineLayout,
+                paint: {
+                  'line-color': deselectedColor,
+                  'line-width': 3,
+                },
+              }]}
+            />
+            {points}
+          </Map>
+        </div>
+      );
+    }
+    return (
+      <div className="none">
+        You must configure a Mapbox key to display map facets.
       </div>
     );
   }
