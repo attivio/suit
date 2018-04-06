86df1730a64ae2e363dce1fcfd59ae5ece8da2bf
Luis Valdez <lvaldez@attivio.com>
2018-04-06 18:41:39 -0400

    PLAT-41529: Move page defs to a separate file for easier coding

--- docs/components/Wizard.md
+++ docs/components/Wizard.md
@@ -4,31 +4,18 @@
 __1:__ Simple wizard.
 
 ```jsx
+  const wizardPages = require('../sampleData/WizardPageDefinitions').default;
   const { Button } = require('react-bootstrap');
   const WizardPage = require('../../src/components/WizardPage').default;
   <div style={{ height: '600px' }}>
     <Wizard
-      pages={[
-        (
-          <WizardPage pageKey="one" title="Step 1">
-            <Button bsStyle="primary">Wizard Button</Button>
-          </WizardPage>
-        ), (
-          <WizardPage pageKey="two" title="Step 2">
-            <Button bsStyle="warning">Iffy Button</Button>
-          </WizardPage>
-        ), (
-          <WizardPage pageKey="three" title="Step 3">
-            <Button bsStyle="danger">Bad Button</Button>
-          </WizardPage>
-        ), (
-          <WizardPage pageKey="four" title="Step 4">
-            <div>Just some text</div>
-          </WizardPage>
-        ),
-      ]}
+      pages={wizardPages}
       onFinish={(values) => {
-        alert('Wizard called to finish!');
+        if (values) {
+          alert('Wizard called to finish!');
+        } else {
+          alert('Wizard called to cancel!');
+        }
       }}
       show
     />
---
+++ docs/sampleData/WizardPageDefinitions.js
@@ -0,0 +1,51 @@
+// @flow
+
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import WizardPage from '../../src/components/WizardPage';
+
+const wizardPages = [
+  (<WizardPage pageKey="type" title="Type">
+    <select size={9}>
+      <option>AVRO Files</option>
+      <option>CSV Files</option>
+      <option>Excel Files</option>
+      <option>JDBC Database</option>
+      <option>ORC Files</option>
+      <option>Parquet Files</option>
+      <option>Web Crawler</option>
+      <option>Windows File System</option>
+      <option>XML Files</option>
+    </select>
+  </WizardPage>
+  ),
+  (<WizardPage pageKey="describe" title="Describe">
+    <div>
+      Name: <input type="text" placeholder="Enter the connector name" />
+    </div>
+    <div>
+      Description: <textarea placeholder="Describe the connector" size={10} />
+    </div>
+  </WizardPage>
+  ),
+  (<WizardPage pageKey="configure" title="Configure">
+    <Button bsStyle="danger">Bad Button</Button>
+  </WizardPage>
+  ), (<WizardPage pageKey="map" title="Map">
+    <div>Just some text</div>
+  </WizardPage>
+  ), (<WizardPage pageKey="enrich" title="Enrich">
+    <div>Just some text</div>
+  </WizardPage>
+  ), (<WizardPage pageKey="process" title="Process">
+    <div>Just some text</div>
+  </WizardPage>
+  ), (<WizardPage pageKey="secure" title="Secure">
+    <div>Just some text</div>
+  </WizardPage>
+  ), (<WizardPage pageKey="schedule" title="Schedule">
+    <div>Just some text</div>
+  </WizardPage>
+  ),
+];
+export default wizardPages;
