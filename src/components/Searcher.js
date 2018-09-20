85f60d1c9cdefb7b95e1a3d3f4c79e43e7e4e3c6
Luis Valdez <lvaldez@attivio.com>
2018-09-20 16:14:08 -0400

    PLAT-41827: cleaning up a couple more lint errors

--- src/components/Searcher.js
+++ src/components/Searcher.js
@@ -68,7 +68,7 @@ type SearcherProps = {
   facets: Array<string>;
   /** The list of relevancy models to use when performing the search. Defaults to the 'default' model. */
   relevancyModels: Array<string>;
-  /** The maxumum number of facets for FacetFinder to add. Defaults to 0, which means FF is disabled. */
+  /** The maximum number of facets for FacetFinder to add. Defaults to 0, which means FF is disabled. */
   facetFinderCount: number;
   /** An optional filter to apply to all queries. */
   queryFilter?: string | null;
@@ -310,7 +310,7 @@ class Searcher extends React.Component<SearcherDefaultProps, SearcherProps, Sear
   }
 
   /**
-   * Conveert an array of stringified facet filters to an array of actual FacetFilter objects.
+   * Convert an array of stringified facet filters to an array of actual FacetFilter objects.
    */
   static deserializeFacetFilters(facetFilters: Array<string>): Array<FacetFilter> {
     return facetFilters.map((facetFilterString: string): FacetFilter => {
@@ -469,6 +469,22 @@ class Searcher extends React.Component<SearcherDefaultProps, SearcherProps, Sear
   }
 
   /**
+   * Set the query string to the passed-in value and trigger the
+   * query immediately, resetting parameters to the beginning.
+   * This is similar to performQueryImmediately() except that the
+   * current value of the queryLanguage is preserved.
+   */
+  setQueryAndSearch(query: string) {
+    this.updateStateResetAndSearch({
+      haveSearched: true, // Force it to update right now
+      error: undefined,
+      response: undefined,
+      facetFilters: [],
+      query,
+    });
+  }
+
+  /**
    * Check to see if the old and new state differ, only comparing the
    * properties we care about (non-transient ones).
    */
@@ -547,10 +563,10 @@ class Searcher extends React.Component<SearcherDefaultProps, SearcherProps, Sear
   }
 
   /**
-   * Given the query string from the location URL, parse it into the values of a SearcherState
-   * object. Values which are missing are set to their default values. Any values in the
-   * queryString which don't apply to the SearcherState are ignored.
-   */
+ * Given the query string from the location URL, parse it into the values of a SearcherState
+ * object. Values which are missing are set to their default values. Any values in the
+ * queryString which don't apply to the SearcherState are ignored.
+ */
   parseLocationQueryStringToState(queryString: string): SearcherState {
     const parsed = QueryString.parse(queryString);
 
@@ -641,7 +657,7 @@ class Searcher extends React.Component<SearcherDefaultProps, SearcherProps, Sear
     // DEFAULT: this.props.format
     let format: 'list' | 'usercard' | 'doccard' | 'debug' | 'simple' = this.props.format;
     if (parsed.format === 'list' || parsed.format === 'usercard' || parsed.format === 'doccard' ||
-        parsed.format === 'debug' || parsed.format === 'simple') {
+      parsed.format === 'debug' || parsed.format === 'simple') {
       format = parsed.format;
     }
 
@@ -726,92 +742,6 @@ class Searcher extends React.Component<SearcherDefaultProps, SearcherProps, Sear
   }
 
   /**
-   * Perform a custom search given a query request. Calls the updateResults callback
-   * and doesn't affect the state of the searcher itself.
-   */
-  doCustomSearch(request: SimpleQueryRequest, updateResults: (response: QueryResponse | null, error: string | null) => void) {
-    this.search.search(request, updateResults);
-  }
-
-  /**
-   * Completely reset the searcher to its default state and call an
-   * optional callback when done.
-   */
-  reset(callback: () => void = () => {}) {
-    this.setState(this.getDefaultState(), callback);
-
-    const callBackWrapper = () => {
-      this.updateStateResetAndSearch(this.getDefaultState());
-    };
-    this.setState({
-      haveSearched: false,
-      response: undefined,
-      error: undefined,
-    }, callBackWrapper);
-  }
-
-  /**
-   * Trigger a new search.
-   *
-   * If the search has been reset by one of the other
-   * methods, then
-   * <ul>
-   *  <li>The "haveSearched" flag is reset to false until the search completes</li>
-   *  <li>The offset is reset to 0 to show the first page of results</li>
-   *  <li>Any facet filters that were applied are cleared.</li>
-   *  <li>Any response or error from a previous search are cleared.</li>
-   * </ul>
-   */
-  doSearch() {
-    const qr = this.getQueryRequest();
-    this.search.search(qr, (response: QueryResponse | null, error: string | null) => {
-      this.updateSearchResults(response, error);
-
-      // potentially do window.scrollTo(0, 0)?
-
-      // Update the URL if needed.
-      const oldQueryString = this.props.location.query;
-      const updatedQueryString = this.generateLocationQueryStringFromState(this.state, oldQueryString);
-      if (oldQueryString !== updatedQueryString) {
-        this.props.history.push(`?${updatedQueryString}`);
-      }
-    });
-  }
-
-  /**
-   * Set the query string to the passed-in value and trigger the
-   * query immediately, resetting parameters to the beginning. The
-   * query is specified as either simple or advanced based on the
-   * value of the advanced flag (it's previous value is ignored).
-   */
-  performQueryImmediately(query: string, advanced: boolean = false) {
-    this.updateStateResetAndSearch({
-      haveSearched: true, // Force it to update right now
-      error: undefined,
-      response: undefined,
-      queryLanguage: advanced ? 'advanced' : 'simple',
-      facetFilters: [],
-      query,
-    });
-  }
-
-  /**
-   * Set the query string to the passed-in value and trigger the
-   * query immediately, resetting parameters to the beginning.
-   * This is similar to performQueryImmediately() except that the
-   * current value of the queryLanguage is preserved.
-   */
-  setQueryAndSearch(query: string) {
-    this.updateStateResetAndSearch({
-      haveSearched: true, // Force it to update right now
-      error: undefined,
-      response: undefined,
-      facetFilters: [],
-      query,
-    });
-  }
-
-  /**
    * Set whether the simple or advanced query language should be
    * used to perform the search.
    * This causes subsequent searches to be reset.
@@ -886,11 +816,82 @@ class Searcher extends React.Component<SearcherDefaultProps, SearcherProps, Sear
   }
 
   /**
+   * Trigger a new search.
+   *
+   * If the search has been reset by one of the other
+   * methods, then
+   * <ul>
+   *  <li>The "haveSearched" flag is reset to false until the search completes</li>
+   *  <li>The offset is reset to 0 to show the first page of results</li>
+   *  <li>Any facet filters that were applied are cleared.</li>
+   *  <li>Any response or error from a previous search are cleared.</li>
+   * </ul>
+   */
+  doSearch() {
+    const qr = this.getQueryRequest();
+    this.search.search(qr, (response: QueryResponse | null, error: string | null) => {
+      this.updateSearchResults(response, error);
+
+      // potentially do window.scrollTo(0, 0)?
+
+      // Update the URL if needed.
+      const oldQueryString = this.props.location.query;
+      const updatedQueryString = this.generateLocationQueryStringFromState(this.state, oldQueryString);
+      if (oldQueryString !== updatedQueryString) {
+        this.props.history.push(`?${updatedQueryString}`);
+      }
+    });
+  }
+
+  /**
    * Add a query filter (in AQL) to the query request.
    */
   addGeoFilter(filter: string) {
     this.addGeoFilters([filter]);
   }
+
+  /**
+   * Perform a custom search given a query request. Calls the updateResults callback
+   * and doesn't affect the state of the searcher itself.
+   */
+  doCustomSearch(request: SimpleQueryRequest, updateResults: (response: QueryResponse | null, error: string | null) => void) {
+    this.search.search(request, updateResults);
+  }
+
+  /**
+   * Completely reset the searcher to its default state and call an
+   * optional callback when done.
+   */
+  reset(callback: () => void = () => {}) {
+    this.setState(this.getDefaultState(), callback);
+
+    const callBackWrapper = () => {
+      this.updateStateResetAndSearch(this.getDefaultState());
+    };
+    this.setState({
+      haveSearched: false,
+      response: undefined,
+      error: undefined,
+    }, callBackWrapper);
+  }
+
+  /**
+   * Set the query string to the passed-in value and trigger the
+   * query immediately, resetting parameters to the beginning. The
+   * query is specified as either simple or advanced based on the
+   * value of the advanced flag (it's previous value is ignored).
+   */
+  performQueryImmediately(query: string, advanced: boolean = false) {
+    this.updateStateResetAndSearch({
+      haveSearched: true, // Force it to update right now
+      error: undefined,
+      response: undefined,
+      queryLanguage: advanced ? 'advanced' : 'simple',
+      facetFilters: [],
+      query,
+    });
+  }
+
 
   /**
    * Add multiple query filters (in AQL) to the query request.
