1b30956e07f5777391650bdd6d4e78d9bf205a46
Luis Valdez <lvaldez@attivio.com>
2018-10-24 12:14:09 -0400

    PLAT-42610: Initial pass at build scripts

---
+++ scripts/publish.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Script to publish the SUIT library to NPM. Will also tag the
+# repositoy with information about the new build.
+# The user must be logged into NPM in order to do this
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+TAG_NAME=
+
+echo "Going to build and publish version $PACKAGE_VERSION of the SUIT library."
+
+echo "Building..."
+npm run fullbuild && \
+echo "Publishing..." && \
+npm publish && \
+echo "Tagging..." && \
+git tag -a $TAG_NAME -m "Published version $PACKAGE_VERSION of SUIT to NPM."
+
+echo "Done. Version $PACKAGE_VERSION of SUIT is now available for use by applications."
---
+++ scripts/updateversion.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Script to update the version of the SUIT library
+# The local repository must be free of changes in order for this to
+# work.
+
+# If no parameter is passed, assume we're updating the patch version
+VERSION_TYPE=$1
+if [ -z "$VERSION_TYPE" ]
+  then
+    VERSION_TYPE = "patch"
+fi
+git pull && \
+npm version $VERSION_TYPE
+#git commit --no-verify && \
+echo "Not pushing yet..."
+#git push
+echo "Updated SUIT version. Run 'npm run fullbuild' and 'npm run publish' to add the new version of the library to the NPM repo."
