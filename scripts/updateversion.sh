495ebb094aa0474e82ce38f5e82e388447daf244
Luis Valdez <lvaldez@attivio.com>
2018-10-31 15:39:44 -0400

    PLAT-42610: Fixing version script

--- scripts/updateversion.sh
+++ scripts/updateversion.sh
@@ -11,15 +11,12 @@ if [ -z "$VERSION_TYPE" ]
     VERSION_TYPE="patch"
 fi
 git pull && \
-npm version $VERSION_TYPE
-#git commit --no-verify && \
-echo "Not pushing yet..."
-git push
-
+npm version $VERSION_TYPE && \
+git commit --no-verify && \
+git push && \
 PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
-
+  | sed 's/[",]//g') && \
 echo "Updated SUIT version to $PACKAGE_VERSION. Run 'npm run fullbuild' and 'npm run publish' to add the new version of the library to the NPM repo."
