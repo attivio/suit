0437e54a7a0cc3a2fee8e021fb60d9b1b71e2de8
Luis Valdez <lvaldez@attivio.com>
2018-12-07 13:15:12 -0500

    PLAT-42834: Fix broken test

--- tests/util/DateUtils-test.js
+++ tests/util/DateUtils-test.js
@@ -4,7 +4,7 @@ import DateUtils from 'src/util/DateUtils';
 
 describe('Test DateUtils', () => {
   it('Can format dates', () => {
-    const d = Date.UTC(1963, 10, 22, 18, 30);
+    const d = new Date(1963, 10, 22, 18, 30);
     expect(DateUtils.formatDate(d, DateFormat.ATTIVIO)).toEqual('1963-11-22T13:30:00-05:00');
     expect(DateUtils.formatDate(d, DateFormat.ISO_8601)).toEqual('1963-11-22T13:30:00-05:00');
     expect(DateUtils.formatDate(d, DateFormat.SHORT_DATE)).toEqual('11/22/1963');
@@ -49,12 +49,12 @@ describe('Test DateUtils', () => {
   });
 
   it('Can format dates in a custom way', () => {
-    const d = Date.UTC(1963, 10, 22, 18, 30);
+    const d = new Date(1963, 10, 22, 18, 30);
     expect(DateUtils.formatDateCustom(d, 'dddd, MMMM Do \\at HH:mm (Qo \\qu\\art\\er)')).toEqual('Friday, November 22nd at 13:30 (4th quarter)'); // eslint-disable-line max-len
   });
 
   it('Can parse an Attivio date string to a date object', () => {
-    const d = Date.UTC(1963, 10, 22, 18, 30);
+    const d = new Date(1963, 10, 22, 18, 30);
     const parsed = DateUtils.stringToDate('1963-11-22T13:30:00-05:00');
     expect(parsed.getTime()).toEqual(d);
   });
