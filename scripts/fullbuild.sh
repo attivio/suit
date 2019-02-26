1885f00a906f09e182d7619ff554489c86b1fc2d
Luis Valdez <lvaldez@attivio.com>
2019-02-26 15:53:12 -0500

    PLAT-43145,PLAT-43146,PLAT-43147: Add project config stuff to help unify and streamline development

--- scripts/fullbuild.sh
+++ scripts/fullbuild.sh
@@ -8,7 +8,7 @@ rm -rf  node_modules && \
 rm -rf es && \
 rm -rf lib && \
 rm -rf styleguide && \
-npm install && \
+npm ci && \
 npm run build && \
 npm run buildflow && \
 npm run validatecomponents && \
