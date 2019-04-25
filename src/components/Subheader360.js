5d93eb40f7a7780e9cd9e6da650fdbec52a73987
Nikhil <thiskidnamednikhil@gmail.com>
2019-04-25 14:55:04 -0400

    Added information tooltip for Subheader360

--- src/components/Subheader360.js
+++ src/components/Subheader360.js
@@ -1,15 +1,51 @@
 // @flow
 
 import React from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 type Subheader360Props = {
+  /**
+   * The text that should be used for the subheader
+   */
   label: string;
+  /**
+   * If true, shows an information icon next to the subheader
+   */
+  description: boolean;
+  /**
+   * The text that should be shown in the tooltip when the information icon is hovered over
+   */
+  descriptionText?: string;
 };
 
-export default class Subheader360 extends React.Component<void, Subheader360Props, void> {
+type Subheader360DefaultProps = {
+  description: boolean;
+  descriptionText?: string;
+};
+
+export default class Subheader360 extends React.Component<Subheader360DefaultProps, Subheader360Props, void> {
+  static defaultProps = {
+    description: false,
+    descriptionText: 'No description text provided',
+  };
+
   static displayName = 'Subheader360';
 
   render() {
-    return <h2 className="attivio-360-subhed">{this.props.label}</h2>;
+    const subheader = this.props.description ?
+      (
+        <div>
+          <h2 className="attivio-360-subhed">
+            {this.props.label}
+            <OverlayTrigger overlay={<Tooltip> {this.props.descriptionText} </Tooltip>}>
+              <img src="img/info.png" alt="Info" style={{ width: '1em', margin: '0em 0em 0em 0.5em' }} />
+            </OverlayTrigger>
+          </h2>
+        </div>
+      ) : (
+        <h2 className="attivio-360-subhed">{this.props.label}</h2>
+      );
+
+    return subheader;
   }
 }
