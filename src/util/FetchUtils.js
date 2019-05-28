21661e8dd769557217764ea1254c433907dacb72
Kristi <kking@attivio.com>
2019-05-28 09:09:30 -0400

    bug/PLAT-43324: Remove irrelevant FIXME comment

--- src/util/FetchUtils.js
+++ src/util/FetchUtils.js
@@ -47,8 +47,6 @@ export default class FetchUtils {
                 // page to let the user log in again.
                 window.location.reload();
               } else if (text.startsWith('<html') && text.includes('j_security_check')) {
-                 // FIXME: This is triggered whenever the api call returns login because the user's session has ended.
-                //  Instead of throwing an error, check if html for login was returned and redirect user to login.
                 window.location.reload();
               } else {
                 // We've received some other sort of error, so let's just log it and stop.
