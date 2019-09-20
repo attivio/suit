0929bf72470a02dbe28b464623e7a059192cbd30
tejas.shah <tejas.shah@attivio.com>
2019-09-20 17:31:53 -0400

    PLAT-44570 : Add test cases for Facet

--- tests/components/ToggleSwitch-test.js
+++ tests/components/ToggleSwitch-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, within } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import ToggleSwitch from '../../src/components/ToggleSwitch';
 
